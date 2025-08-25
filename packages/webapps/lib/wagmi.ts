b21587e90dde19e7f0092127f7a9ee2323449f36
RowanWang7 <87600024+RowanWang6@users.noreply.github.com>
2025-08-25 16:55:19 +0800

    合并

--- packages/webapps/lib/wagmi.ts
+++ packages/webapps/lib/wagmi.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia, hardhat } from 'wagmi/chains';
-=======
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia, hardhat } from "wagmi/chains";
 import { http } from "wagmi";
->>>>>>> upstream/main
 
 export const config = getDefaultConfig({
   appName: 'intensivesquad',
